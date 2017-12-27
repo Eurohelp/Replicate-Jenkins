e542076664e13b78e5422dfdbe8f35341a90b869
Mishel Uchuari <dmuv7@hotmail.com>
2017-12-27 13:14:14 +0100

    archivo .sh modificado

--- script-to-execute.sh
+++ script-to-execute.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-runJenkins(){
+clear
 echo "Se pone en marcha jenkins"
 ./usr/local/bin/jenkins.sh &>/dev/null &
 bg
-}
-runPipeline(){
+
 sleep 60
 echo "Se ejecuta el pipeline"
 jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini update /etc/jenkins_jobs/scm_pipeline.yaml
-}
-clear
-runJenkins
-runPipeline
 
-#chmod 777 /etc/jenkins_jobs/script.sh
-#./etc/jenkins_jobs/script.sh
